736aec5897da4560fe152b58b872f93b359bc7e8
JoseManuelCruzSanchez <44446487+JoseManuelCruzSanchez@users.noreply.github.com>
2019-01-14 16:23:56 +0100

    Add files via upload

---
+++ Calendar_Produccion_Javascript_Ajax_PHP/version_antes_seleccionar_eventos_subpaginas/datos_desde_google_1.php
@@ -0,0 +1,16 @@
+<?php
+$ID_del_Calendario = "";
+$apikey = "";
+$fechaParametroAPI = $_GET['fecha'];
+$urlConsultaAPI = 'https://www.googleapis.com/calendar/v3/calendars/' . $ID_del_Calendario . '/events?key=' . $apikey . '&singleEvents=true&timeMin='. $fechaParametroAPI . '&maxResults=4&orderBy=starttime';
+
+$curlSession = curl_init();
+curl_setopt($curlSession, CURLOPT_URL, $urlConsultaAPI);
+curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
+curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
+
+//$jsonData = json_decode(curl_exec($curlSession));
+echo curl_exec($curlSession);
+curl_close($curlSession);
+
+?>
